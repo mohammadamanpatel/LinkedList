5ac1390e160d1a4dab5e8e2d42e6490515b73e1f
mohammadamanpatel <125564920+mohammadamanpatel@users.noreply.github.com>
2023-05-16 17:00:55 +0530

    Copy List with Random Pointer(with Map).cpp

--- Copy List with Random Pointer(with Map).cpp
+++ Copy List with Random Pointer(with Map).cpp
@@ -6,7 +6,7 @@ public:
         Node*newNode = new Node(head->val);
         //agar me old node ke map se newNode ko joint karu to ek connection
         // jisse newnode bhi usi direction me                        
-        //point karega jese old node karta tha
+        //point karega jese old node karta tha ye ham random ptrs ko point karaane ke liye kar rhe hai
         //basically puraani node se nayi node ki mapping hai
         mp[head] = newNode; 
         newNode->next = copiedList(head->next,mp);
