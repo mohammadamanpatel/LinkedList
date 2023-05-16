c6a86afaaa7b00d84725a796a8ad6304ad343d3a
mohammadamanpatel <125564920+mohammadamanpatel@users.noreply.github.com>
2023-05-16 16:18:37 +0530

    Copy List with Random Pointer(with Map).cpp

---
+++ Copy List with Random Pointer(with Map).cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    Node*copiedList(Node*head,unordered_map<Node*,Node*>&mp)
+    {
+        if(!head) return 0;
+        Node*newNode = new Node(head->val);
+        //agar me old node ke map se newNode ko joint karu to ek connection
+        // jisse newnode bhi usi direction me                        
+        //point karega jese old node karta tha
+        //basically puraani node se nayi node ki mapping hai
+        mp[head] = newNode; 
+        newNode->next = copiedList(head->next,mp);
+        if(head->random)
+        {
+           newNode->random = mp[head->random];    
+        }     
+        return newNode;               
+    }
+    Node* copyRandomList(Node* head) {
+        unordered_map<Node*,Node*>mp;
+        return copiedList(head,mp);
+    }
+};
